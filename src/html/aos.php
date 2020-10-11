5c30c42dfd5184abceb469c7f367ac2ecacca6a5
MayNiklas <niklas.steffen@uni-bonn.de>
2020-10-11 13:59:26 +0200

    Implemented css file into php

--- src/html/aos.php
+++ src/html/aos.php
@@ -1,35 +1,48 @@
-<?php
-$episode = $_GET["episode"];
-$episode = $_GET["season"]."x".$episode;
+<!DOCTYPE html>
+  <html>
+    <head>
+      <link rel="stylesheet" href="style.css">
+      <title>scene-extractor</title>
+    </head>
+    <body>
+      <header>
+        <h1>Scenes extractor</h1>
+      </header>
+      <main>
+      	<?php
+      	$episode = $_GET["episode"];
+      	$episode = $_GET["season"]."x".$episode;
 
-$startm = $_GET["start-m"];
-$starts = $_GET["start-s"];
-$endm = $_GET["end-m"];
-$ends = $_GET["end-s"];
+      	$startm = $_GET["start-m"];
+      	$starts = $_GET["start-s"];
+      	$endm = $_GET["end-m"];
+      	$ends = $_GET["end-s"];
 
-$start = ((60*$startm)+$starts);
-$end = (((60*$endm)+$ends)-$start);
+      	$start = ((60*$startm)+$starts);
+      	$end = (((60*$endm)+$ends)-$start);
 
+      	$pfad = "/source/Marvel's Agents of S.H.I.E.L.D/";
 
-$pfad = "/source/Marvel's Agents of S.H.I.E.L.D/";
+      	$list="cd \"$pfad\" && ls -d */* | grep $episode";
+      	$video=exec("$list");
+      	$videolink="\"$pfad$video\"";
 
+      	$link_address = "/dl/aos-$episode-$startm.$starts-$endm.$ends.mp4";
 
-$list="cd \"$pfad\" && ls -d */* | grep $episode";
-$video=exec("$list");
-$videolink="\"$pfad$video\"";
+      	$cmd = "HandBrakeCLI -q 24 --optimize --encoder x264 --start-at duration:$start --stop-at duration:$end -i $videolink -o /var/www/html$link_address";
+      	exec("$cmd");
 
-$link_address = "/dl/aos-$episode-$startm.$starts-$endm.$ends.mp4";
+      	$changetitle = "exiftool -all= -overwrite_original /var/www/html$link_address";
+      	exec ("$changetitle");
 
-$cmd = "HandBrakeCLI -q 24 --optimize --encoder x264 --start-at duration:$start --stop-at duration:$end -i $videolink -o /var/www/html$link_address";
-exec("$cmd");
-
-$changetitle = "exiftool -all= -overwrite_original /var/www/html$link_address";
-exec ("$changetitle");
-
-echo "<p>$video</p>";
-echo "<p>$startm:$starts - $endm:$ends</p>";
-echo "<a href='$link_address'>aos.de$link_address</a>";
-?>
-
-
+      	echo "<p>$video</p>";
+      	echo "<p>$startm:$starts - $endm:$ends</p>";
+      	echo "<a href='$link_address'>aos.de$link_address</a>";
+      	?>
+      </main>
+      <footer>
+        <p>For personal use only!</p>
+      </footer>
+      </body>
+  </html>
 
